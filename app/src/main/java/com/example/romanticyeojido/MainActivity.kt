1074744eedad875c820f83bf757da68825517c24
Roy <onlymylife2014@gmail.com>
2024-12-19 20:04:32 +0900

    Feat: 지도 API 추가 (#28)

--- app/src/main/java/com/example/romanticyeojido/MainActivity.kt
+++ app/src/main/java/com/example/romanticyeojido/MainActivity.kt
@@ -2,9 +2,11 @@ package com.example.romanticyeojido
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.example.romanticyeojido.databinding.ActivityMainBinding
 import com.example.romanticyeojido.ui.map.MapActivity
+import com.kakao.sdk.common.util.Utility
 
 class MainActivity : AppCompatActivity() {
 
@@ -15,6 +17,9 @@ class MainActivity : AppCompatActivity() {
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val keyHash = Utility.getKeyHash(this)
+        Log.d("Hash", keyHash)
 
         binding.mapCardShortcut.setOnClickListener {
             startActivity(Intent(this, MapActivity::class.java))
